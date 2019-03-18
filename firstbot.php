939dc1326a255790706f163724704bf52284cb52
phan111 <chatpimookt@gmail.com>
2019-03-18 14:09:38 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -26,6 +26,7 @@
 	$events = json_decode($content, true);//แปลง json เป็น php
 	if (!is_null($events['events'])) //check ค่าในตัวแปร $events
 	{
+		echo "BOT OK";
 		foreach ($events['events'] as $event) {
 			if ($event['type'] == 'message' && $event['message']['type'] == 'text')
 			{
@@ -36,4 +37,4 @@
 			}
 		}
 	}
-	echo "BOT OK";
+	
