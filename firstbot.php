f840dd95149d0e664c03de9c12a5e600f6f54bf6
phan111 <chatpimookt@gmail.com>
2019-03-18 14:15:25 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -1,8 +1,7 @@
 <?php
 	function reply_msg($txtin,$replyToken)//สร้างข้อความและตอบกลับ
 	{
-		$access_token = 'JlqfkhQinH+BmTxFyFOyOjMLtiPY33oUH49gMD8yae2yvLBcbJiREM5kMAyCt263s+MG0mew1M1Sak1073mHbHkRMvayt9UhvofIOBtfIGa/GWbx7fbjB+DdkCjrZsQWkflrBHhdsazEIeblZh3wtgdB04t89/1O/w1cDnyilFU=
-';
+		$access_token = 'JlqfkhQinH+BmTxFyFOyOjMLtiPY33oUH49gMD8yae2yvLBcbJiREM5kMAyCt263s+MG0mew1M1Sak1073mHbHkRMvayt9UhvofIOBtfIGa/GWbx7fbjB+DdkCjrZsQWkflrBHhdsazEIeblZh3wtgdB04t89/1O/w1cDnyilFU=';
 		$messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
 		$url = 'https://api.line.me/v2/bot/message/reply';
 		$data = [
