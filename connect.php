ee4866e14971a796b8775b9d2a65b24d62ec0d73
phan111 <chatpimookt@gmail.com>
2019-04-09 14:52:50 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,12 @@
 <?php
-  $server = "us-cdbr-iron-east-03.cleardb.net";
-  $username = "bcc62c80f44ddb";
-  $password = "13c350a1";
-  $db = "heroku_31f1dcbaacb6adb";
-  $conn = new mysqli($server, $username, $password, $db);
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  }else{
-    echo "db connected!";
+$server = "us-cdbr-iron-east-03.cleardb.net";
+$username = "bcc62c80f44ddb";
+$password = "13c350a1";
+$db = "heroku_31f1dcbaacb6adb";
+$con=mysqli_connect($server, $username, $password, $db);
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-  mysqli_query($conn, "SET NAMES utf8");
 ?>
