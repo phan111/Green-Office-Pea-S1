3b520406aa7c5c2a50408a5dee062c72d2141c29
phan111 <chatpimookt@gmail.com>
2019-04-09 13:56:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
-echo "string ";
-?>
+	function reply_msg($txtin,$replyToken)//สร้างข้อความและตอบกลับ
+	{
+		$access_token = 'JlqfkhQinH+BmTxFyFOyOjMLtiPY33oUH49gMD8yae2yvLBcbJiREM5kMAyCt263s+MG0mew1M1Sak1073mHbHkRMvayt9UhvofIOBtfIGa/GWbx7fbjB+DdkCjrZsQWkflrBHhdsazEIeblZh3wtgdB04t89/1O/w1cDnyilFU=';
+		$messages = ['type' => 'text','text' => $txtin];//สร้างตัวแปร 
+		$url = 'https://api.line.me/v2/bot/message/reply';
+		$data = [
+					'replyToken' => $replyToken,
+					'messages' => [$messages],
+				];
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		echo $result . "\r\n";
+	}
+	// รับข้อมูล
+	$content = file_get_contents('php://input');//รับข้อมูลจากไลน์
+	$events = json_decode($content, true);//แปลง json เป็น php
+	if (!is_null($events['events'])) //check ค่าในตัวแปร $events
+	{
+		foreach ($events['events'] as $event) {
+			if ($event['type'] == 'message' && $event['message']['type'] == 'text')
+			{
+				$replyToken = $event['replyToken']; //เก็บ reply token เอาไว้ตอบกลับ
+				$source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
+				$txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
+				reply_msg($content,$replyToken);      
+			}
+		}
+	}
+	echo "BOT OK";
