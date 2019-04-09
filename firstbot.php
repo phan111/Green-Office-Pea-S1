16ca5e5d90e71a7e719ffc5e05c4508300cb3581
phan111 <chatpimookt@gmail.com>
2019-04-09 14:29:32 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -1,5 +1,5 @@
 <?php
-	include 'connect.php';
+	//include 'connect.php';
 	function reply_msg($txtin,$replyToken)//สร้างข้อความและตอบกลับ
 	{
 		$access_token = 'JlqfkhQinH+BmTxFyFOyOjMLtiPY33oUH49gMD8yae2yvLBcbJiREM5kMAyCt263s+MG0mew1M1Sak1073mHbHkRMvayt9UhvofIOBtfIGa/GWbx7fbjB+DdkCjrZsQWkflrBHhdsazEIeblZh3wtgdB04t89/1O/w1cDnyilFU=';
