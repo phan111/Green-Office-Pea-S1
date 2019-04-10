aa38d32937e01abc8fef1640d57069b5a55cadd7
phan111 <chatpimookt@gmail.com>
2019-04-10 14:31:14 +0700

    Create add.php

---
+++ add.php
@@ -0,0 +1,11 @@
+<?php
+include 'connect.php';
+$sql = "INSERT INTO line_reply (msg, reply)
+VALUES ('นโยบาย', 'line://app/1556091170-O9nZo3E3')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
