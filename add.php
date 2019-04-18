10b3e708872ce18b1b9075281eda9dc8541dd708
phan111 <chatpimookt@gmail.com>
2019-04-18 16:12:54 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,11 +1,53 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (21, '10 วิธีลดโลกร้อน', 'line://app/1556091170-mZRWD4V4', 8)";
+VALUES (22, 'คณะกรรมการฝ่ายบริหาร', 'line://app/1556091170-01BlEQLQ', 9)";
 if ($con->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New record created successfully<br>";
 } else {
-    echo "Error: " . $sql . "<br>" . $con->error;
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (23, 'หมวด 1', 'line://app/1556091170-wp1aGQNQ', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (24, 'หมวด 2', 'line://app/1556091170-7vlAYwXw', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (25, 'หมวด 3', 'line://app/1556091170-AVNPZJzJ', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (26, 'หมวด 4', 'line://app/1556091170-jXq4w2m2', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (27, 'หมวด 5', 'line://app/1556091170-NLWJKpnp', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
+VALUES (28, 'หมวด 6', 'line://app/1556091170-zoGRQlPl', 9)";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
 }
 
 ?>
