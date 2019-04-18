ddf4647ad218c48533d3dab4b2bc52d1f6d7cfd6
phan111 <chatpimookt@gmail.com>
2019-04-18 09:52:00 +0700

    Create tbl_keyword.php

---
+++ tbl_keyword.php
@@ -0,0 +1,9 @@
+<?php
+include 'conncect.php';
+$sql = "SELECT * FROM keyword";
+$result = mysqli_query($con, $sql);
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+foreach($row as $key){
+echo $key->keyword_id.' '.$key->keyword.' '.$key->topic.'<br>';
+}
+?>
