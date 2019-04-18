e17b5824eeaec4b0c1faadca02e4e2115f92830c
phan111 <chatpimookt@gmail.com>
2019-04-18 11:18:23 +0700

    Update tbl.php

--- tbl.php
+++ tbl.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 echo $_GET['tbl'];
-$sql = "SELECT * FROM '".$_GET['tbl']."'";
+$sql = "SELECT * FROM ".$_GET['tbl'];
 $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo '<pre>';
