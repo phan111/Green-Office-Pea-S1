9fbbeedf70bbbbd4e85c36b9e873c0c9de86315d
phan111 <chatpimookt@gmail.com>
2019-04-18 10:08:21 +0700

    Update tbl_keyword.php

--- tbl_keyword.php
+++ tbl_keyword.php
@@ -5,6 +5,7 @@ $sql = "SELECT * FROM keyword";
 $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo '<pre>';
+print_r($row);
 foreach($row as $key){
   //echo $key->keyword_id.' '.$key->keyword.' '.$key->topic.'<br>';
   print_r($key);
