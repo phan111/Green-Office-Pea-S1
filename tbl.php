fb012d87eb7151928f576aa99938555f64c6f385
phan111 <chatpimookt@gmail.com>
2019-04-18 11:13:37 +0700

    Update tbl.php

--- tbl.php
+++ tbl.php
@@ -7,7 +7,6 @@ $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 echo '<pre>';
 print_r($row);
 foreach($row as $key){
-  //echo $key->keyword_id.' '.$key->keyword.' '.$key->topic.'<br>';
   print_r($key);
 }
 echo '</pre>';
