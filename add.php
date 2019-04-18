9d3c59474fa22b32a620647b409be2587caa60e0
phan111 <chatpimookt@gmail.com>
2019-04-18 16:15:46 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (28, 'โลโก้และคำขวัญ', 'line://app/1556091170-opYG08v8', 11)";
+VALUES (29, 'โลโก้และคำขวัญ', 'line://app/1556091170-opYG08v8', 11)";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
