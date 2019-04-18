c39d9594044a11a234e2c21758fd0bb9d6a16538
phan111 <chatpimookt@gmail.com>
2019-04-18 13:28:40 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO keyword (keyword, topic)
-VALUES ('Greenofficeมาตรการพลังงานทรัพยากรกรีน', 'เป้าหมายและมาตรการพลังงาน/ทรัพยากร (ได้แก่ น้ำ ไฟฟ้า น้ำมัน เชื้อเพลิง ก๊าซหุงต้ม กระดาษ และอื่นๆ)')";
+VALUES ('Greenofficeขยะของเสียมาตรการจัดการของเสียกรีน', 'เป้าหมายและมาตรการจัดการของเสีย')";
 
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully";
