1d1bb7924cfff8308d510706671ff352a0c03232
phan111 <chatpimookt@gmail.com>
2019-04-18 16:05:50 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,35 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (17, 'ก๊าซเรือนกระจกตัวการโลกร้อน', 'line://app/1556091170-8AR3jDMD', 8)";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
-}
-$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (18, 'แหล่งปล่อยก๊าซเรือนกระจก', 'line://app/1556091170-RXg2Vknk', 8)";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
-}
-$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (19, 'ปรากฎการณ์เรือนกระจก', 'line://app/1556091170-nj7xbywy', 8)";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
-}
-$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (20, 'โลกร้อนมีผลกระทบอย่างไร', 'line://app/1556091170-PGmJz7y7', 8)";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error;
-}
-$sql = "INSERT INTO detail (detail_id, subtopic, reply, keyword_id)
-VALUES (20, '10 วิธีลดโลกร้อน', 'line://app/1556091170-mZRWD4V4', 8)";
+VALUES (21, '10 วิธีลดโลกร้อน', 'line://app/1556091170-mZRWD4V4', 8)";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
