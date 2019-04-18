f8e58ad06bd8f6bfe1804a6b657b2d029b9b5963
phan111 <chatpimookt@gmail.com>
2019-04-18 13:43:46 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,19 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO keyword (keyword, topic)
-VALUES ('Greenofficeขยะของเสียมาตรการจัดการของเสียกรีน', 'เป้าหมายและมาตรการจัดการของเสีย')";
+$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (5, 'Greenofficeขยะของเสียมาตรการจัดการของเสียกรีน', 'เป้าหมายและมาตรการจัดการของเสีย')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (6, '', 'ผลการใช้ทรัพยากร พลังงาน และของเสีย')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (7, 'Greenofficeสินค้าบริการที่เป็นมิตรกับสิ่งแวดล้อมกรีน', 'สินค้าและบริการที่เป็นมิตรกับสิ่งแวดล้อม')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (8, 'Greenofficeก๊าซเรือนกระจกกรีน', 'ก๊าซเรือนกระจก')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (9, 'Greenofficeทีมงานและอำนาจหน้าที่แต่ละหมวดกรีน', 'ทีมงานด้านสิ่งแวดล้อม และอำนาจหน้าที่')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (10, '', 'การรณรงค์ในการจัดการสิ่งแวดล้อมอื่นๆ เช่น ความสะอาดและความเป็นระเบียบ เป็นต้น')";
+$sql .= "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (11, 'Greenofficeโลโก้และคำขวัญกรีน', 'โลโก้และคำขวัญ')";
 
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully";
