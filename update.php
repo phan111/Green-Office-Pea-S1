53161118750e9c7a78dcb20560d6b21538fcf52e
phan111 <chatpimookt@gmail.com>
2019-04-18 11:08:04 +0700

    Create update.php

---
+++ update.php
@@ -0,0 +1,12 @@
+<?php
+include 'connect.php';
+$sql = 'UPDATE detail
+        SET reply = "line://app/1556091170-O9nZo3E3"
+        WHERE keyword_id = 1';
+if (mysqli_query($conn, $sql)) {
+   echo "Record updated successfully";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+mysqli_close($conn);
+?>
