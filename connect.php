558b7fbefe5e8f40595d0ff5c5a60e013f323e54
phan111 <chatpimookt@gmail.com>
2019-04-18 10:28:17 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,12 +3,12 @@ $server = "us-cdbr-iron-east-03.cleardb.net";
 $username = "bcc62c80f44ddb";
 $password = "13c350a1";
 $db = "heroku_31f1dcbaacb6adb";
-$con=mysqli_connect($server, $username, $password, $db);
+$con = mysqli_connect($server, $username, $password, $db);
+mysqli_set_charset($con, "utf8");
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()){
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }else{
+}else{
   echo 'connected';
 }
 ?>
