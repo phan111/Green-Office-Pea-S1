6b1f6b075b387ebbe68224c5b66a50237132c2ba
phan111 <chatpimookt@gmail.com>
2019-04-18 09:54:15 +0700

    Update tbl_keyword.php

--- tbl_keyword.php
+++ tbl_keyword.php
@@ -3,7 +3,9 @@ include 'conncect.php';
 $sql = "SELECT * FROM keyword";
 $result = mysqli_query($con, $sql);
 $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+echo '<pre>';
 foreach($row as $key){
-echo $key->keyword_id.' '.$key->keyword.' '.$key->topic.'<br>';
+  //echo $key->keyword_id.' '.$key->keyword.' '.$key->topic.'<br>';
+  print_r($key);
 }
 ?>
