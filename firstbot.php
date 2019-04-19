0ec7c870db32af96263d11313194b7fa12311f09
phan111 <chatpimookt@gmail.com>
2019-04-19 09:38:33 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -119,11 +119,12 @@
 				$source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
 				$txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
 				if (strpos($txtin, '#') !== false) {
+					$trimmed = str_replace('#', '', $txtin)
 					$sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.reply 
 						FROM keyword k
 						INNER JOIN detail d
 						ON k.keyword_id = d.keyword_id
-						WHERE keyword LIKE '%{$txtin}%'
+						WHERE keyword LIKE '%{$trimmed}%'
 					";
 					$resource = $con->query($sql);
 					$ans = array();
