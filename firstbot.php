0d7a53a71aac65a99e92f7695bde94c088efd27d
phan111 <chatpimookt@gmail.com>
2019-04-19 09:34:29 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -118,19 +118,21 @@
 				$replyToken = $event['replyToken']; //เก็บ reply token เอาไว้ตอบกลับ
 				$source_type = $event['source']['type'];//เก็บที่มาของ event(user หรือ group)
 				$txtin = $event['message']['text'];//เอาข้อความจากไลน์ใส่ตัวแปร $txtin
-				$sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.reply 
-					FROM keyword k
-					INNER JOIN detail d
-					ON k.keyword_id = d.keyword_id
-					WHERE keyword LIKE '%{$txtin}%'
-				";
-				$resource = $con->query($sql);
-				$ans = array();
-				while ( $rows = $resource->fetch_assoc() ) {
-				    $ans[] = $rows;
-				    //print_r($rows);//echo "{$row['field']}";
+				if (strpos($txtin, '#') !== false) {
+					$sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.reply 
+						FROM keyword k
+						INNER JOIN detail d
+						ON k.keyword_id = d.keyword_id
+						WHERE keyword LIKE '%{$txtin}%'
+					";
+					$resource = $con->query($sql);
+					$ans = array();
+					while ( $rows = $resource->fetch_assoc() ) {
+					    $ans[] = $rows;
+					    //print_r($rows);//echo "{$row['field']}";
+					}
+					reply_msg($ans, $replyToken);
 				}
-				reply_msg($ans, $replyToken);
 			}
 		}
 	}
