f5b5a5d562759c18779f6357610b80caaeb10c8c
phan111 <chatpimookt@gmail.com>
2019-04-22 11:07:21 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -25,85 +25,155 @@
 	function flex_msg($ans)
 	{
 	$json1 = '{
-				"type":"flex",
-				"altText":"การโต้ตอบของบอท",
-				"contents":{
-  						"type": "bubble",
-  						"hero": {
-   								 "type": "image",
-    								 "url": "https://raw.githubusercontent.com/phan111/Green-Office-Pea-S1/master/Webp.net-resizeimage.png",
-    								 "size": "full",
-      							         "aspectRatio": "16:9",
-    								 "aspectMode": "fit",
-    								 "action": {
-      	   									"type": "uri",
-      										"uri": "line://app/1556091170-O9nZo3E3"
-    									   }
-  							},
-  						"body": {
-    								"type": "box",
-    								"layout": "vertical",
-    								"contents": [
-      										{
-        										"type": "text",
-        										"text": "'.$ans[0]['topic'].'",
-        										"weight": "bold",
-        										"size": "sm"
-      										},
-      										{
-        										"type": "box",
-        										"layout": "vertical",
-        										"margin": "lg",
-        										"spacing": "sm",
-        										"contents": [
-          												{
-            													"type": "box",
-            													"layout": "baseline",
-            													"spacing": "sm",
-            													"contents": [
-															      {
-																"type": "text",
-																"text": "Green Office Bot",
-																"wrap": true,
-																"color": "#d4ed89",
-																"size": "sm",
-																"flex": 5
-															      }
-            														   ]
-          												}
-        											   ]
-      										}
-    									]
-  							},
-						  "footer": {
-							    "type": "box",
-							    "layout": "vertical",
-							    "spacing": "sm",
-							    "contents": [';
-						$count = count($ans);
-						for($i = 0; $i < $count; $i++){
-									$json1 .= '{
-										"type": "button",
-										"style": "primary",
-										"height": "sm",
-										"action": {
-										  "type": "uri",
-										  "label": "'.$ans[$i]['subtopic'].'",
-										  "uri": "'.$ans[$i]['reply'].'"
-										}
-						      			     },';
-						}
-     
-									      $json1 .= '{
-										"type": "spacer",
-										"size": "sm"
-									      }
-    									],
-    							 "flex": 0
-  							}
-				}
-	
-				}';
+  "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": "http://linecorp.com/"
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "Brown Cafe",
+        "weight": "bold",
+        "size": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "margin": "md",
+        "contents": [
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "icon",
+            "size": "sm",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
+          },
+          {
+            "type": "text",
+            "text": "4.0",
+            "size": "sm",
+            "color": "#999999",
+            "margin": "md",
+            "flex": 0
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "lg",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Place",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Time",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": "10:00 - 23:00",
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "CALL",
+          "uri": "https://linecorp.com"
+        }
+      },
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "WEBSITE",
+          "uri": "https://linecorp.com"
+        }
+      },
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
+  }
+}';
 	$result = json_decode($json1);
 	return $result;
 	}
