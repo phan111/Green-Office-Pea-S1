b67a045f649b58e718b0164dda3b84dc70027b2c
phan111 <chatpimookt@gmail.com>
2019-04-30 14:57:17 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,6 +1,6 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO detail (keyword_id, keyword, topic)
+$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
 VALUES (8, 'Greenofficeก๊าซเรือนกระจกกรีน', 'ก๊าซเรือนกระจก')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
