3513c9f4101110a746fb8798a1d644546774ba01
phan111 <chatpimookt@gmail.com>
2019-05-07 14:45:40 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
-  include('https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view?usp=sharing');
+  echo include('https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view?usp=sharing');
 ?>
