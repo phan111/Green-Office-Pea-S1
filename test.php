d9295955b4df079f6b36b343d76b19c4043f5353
phan111 <chatpimookt@gmail.com>
2019-05-07 14:49:04 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
-  echo stream_get_contents(fopen('https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view?usp=sharing'), 'rb');
+  echo file_get_contents('https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view?usp=sharing');
 ?>
