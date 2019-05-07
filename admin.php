a64239e9b089802c1732a4a4acd2b9a918e33673
phan111 <chatpimookt@gmail.com>
2019-05-07 13:46:25 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -63,7 +63,7 @@
 $(document).ready(function(){
   $("#myInput").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
+    $("#myTable tr td input").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
