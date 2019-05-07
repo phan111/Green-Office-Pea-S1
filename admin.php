2680daa906192ddcebcdf7cdd04745abe828fe93
phan111 <chatpimookt@gmail.com>
2019-05-07 12:57:15 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,13 +1,3 @@
-<?php
-  include 'connect.php';
-  $sql = '
-    SELECT k.keyword_id, k.keyword, d.detail_id, d.subtopic, d.reply, d.reply_pc
-    FROM keyword k
-    INNER JOIN detail d
-    ON k.keyword_id = d.keyword_id;
-  ';
-  $resource = $con->query($sql);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -29,33 +19,32 @@
   <table class="table table-bordered">
     <thead>
       <tr>
-        <th>keyword_id</th>
-        <th>detail_id</th>
-        <th>keyword</th>
-        <th>topic</th>
-        <th>subtopic</th>
-        <th>reply</th>
-        <th>reply_pc</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
       </tr>
     </thead>
     <tbody id="myTable">
-    <?php
-    foreach($resource as $key){
-    ?>
       <tr>
-        <td><?=$key['keyword_id']?></td>
-        <td><?=$key['detail_id']?></td>
-        <td><?=$key['keyword']?></td>
-        <td><?=$key['topic']?></td>
-        <td><?=$key['subtopic']?></td>
-        <td><?=$key['reply']?></td>
-        <td><?=$key['reply_pc']?></td>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
       </tr>
-    <?php
-    }
-    $resource->free();
-    $db->close();
-    ?>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@mail.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@greatstuff.com</td>
+      </tr>
+      <tr>
+        <td>Anja</td>
+        <td>Ravendale</td>
+        <td>a_r@test.com</td>
+      </tr>
     </tbody>
   </table>
   
@@ -72,5 +61,6 @@ $(document).ready(function(){
   });
 });
 </script>
+
 </body>
 </html>
