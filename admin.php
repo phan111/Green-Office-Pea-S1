1bf0bfb8971e62b3bdd970e014d431b27c324c39
phan111 <chatpimookt@gmail.com>
2019-05-07 10:30:09 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,40 @@
+<?php
+  include 'connect.php';
+  $sql = '
+    SELECT k.keyword_id, k.keyword, d.detail_id, d.subtopic, d.reply, d.reply_pc
+    FROM keyword k
+    INNER JOIN detail d
+    ON k.keyword_id = d.keyword_id;
+  ';
+  $resource = $con->query($sql);
+  $rows = $resource->fetch_assoc()
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <h2>Simple Collapsible</h2>
+  <a href="#demo" class="btn btn-primary" data-toggle="collapse">Simple collapsible</a>
+  <div id="demo" class="collapse">
+  <?php
+    foreach($rows as $key){
+      print_r($key);
+    }
+    $resource->free();
+    $db->close();
+  ?>
+  </div>
+</div>
+
+</body>
+</html>
