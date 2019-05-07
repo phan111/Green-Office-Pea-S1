1ff3bc1a628499e41ecd4ce7b656268efc3e4e2b
phan111 <chatpimookt@gmail.com>
2019-05-07 15:20:49 +0700

    Update firstbot.php

--- firstbot.php
+++ firstbot.php
@@ -148,7 +148,7 @@
             "action": {
               "type": "uri",
               "label": "'.$ans[$i]['subtopic'].'",
-              "uri": "'.$ans[$i]['reply_pc'].'"
+              "uri": "https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view"
             }
           }
 	';
