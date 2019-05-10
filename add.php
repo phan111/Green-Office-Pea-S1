768b2fb0f8b7bb7b606e0cfecf26236b19bbf740
phan111 <chatpimookt@gmail.com>
2019-05-10 11:18:26 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,6 +1,6 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO keyword (keyword_id, detail_id, subtopic, reply_pc)
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
 VALUES (12, 43, 'ข้อเสนอแนะด้านสิ่งแวดล้อม', 'https://drive.google.com/file/d/1_LIuWsiNOOYe0ogxAi7T-sEKTdL1Hz-O/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
