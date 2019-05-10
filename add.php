abdfb8c9c5464e68a81df37fee2ce59f81ed306b
phan111 <chatpimookt@gmail.com>
2019-05-10 10:59:24 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (3, 34, 'ช่องทางค้นคว้า', 'https://drive.google.com/file/d/17t1BVHZUd4EHe2n6xhPn8mV7L4jYovHV/view?usp=sharing')";
+VALUES (3, 35, 'กฎหมายอัคคีภัย', 'https://drive.google.com/file/d/1ESSljQfPQtHrfMOVTYazfJO82eK9xmPK/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
