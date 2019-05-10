717f22944b8b7aec7592b5e5c7134b35189b0a92
phan111 <chatpimookt@gmail.com>
2019-05-10 11:12:55 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,22 +1,14 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 38, 'รณรงค์ด้านไฟฟ้าปิดปรับปลดเปลี่ยน', 'https://drive.google.com/file/d/1CTYofkG2RTvdWx4clpYTuNmsUpLjfW_U/view?usp=sharing')";
+VALUES (10, 41, 'รณรงค์เลิกสูบบุหรี่ 1', 'https://drive.google.com/file/d/1KVLSyE-xWScZR-ftmjh6y4LLWAw5yP7N/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
     echo "Error: " . $sql . "<br>" . $con->error."<br>";
 }
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 39, 'รณรงค์ด้านไฟฟ้าประหยัดไฟง่ายๆ', 'https://drive.google.com/file/d/1w06BFFKbM1CaVIuvZEUYAc3lUhtf6r2d/view?usp=sharing')";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error."<br>";
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 40, 'รณรงค์ด้านน้ำปิดก๊อกให้สนิท', 'https://drive.google.com/file/d/1z_F-6k-e-qdDgTSWke-ynfIgUmiTjd6a/view?usp=sharing')";
+VALUES (4, 42, 'รณรงค์เลิกสูบบุหรี่ 2', 'https://drive.google.com/file/d/13cPdJKwiS-RN0Sh1iDcXODfzWjeWngOj/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
