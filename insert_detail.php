6f9240940d6670f405c63753506aba4a7da55786
phan111 <chatpimookt@gmail.com>
2019-05-10 10:46:34 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -5,11 +5,10 @@ print_r($_GET);
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
 VALUES ($_GET['keyword_id'], $_GET['detail_id'], $_GET['subtopic'], $_GET['reply_pc'])";
 
-if (mysqli_query($con, $sql)) {
+/*if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+}*/
 
-mysqli_close($conn);
 ?>
