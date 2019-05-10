a3e5c49fcfc3f386896584b7833d6c4b5d999004
phan111 <chatpimookt@gmail.com>
2019-05-10 11:14:45 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,18 +1,12 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (10, 41, 'รณรงค์เลิกสูบบุหรี่ 1', 'https://drive.google.com/file/d/1KVLSyE-xWScZR-ftmjh6y4LLWAw5yP7N/view?usp=sharing')";
+$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (12, 'Greenofficeข้อเสนอแนะข้อร้องเรียนด้านสิ่งแวดล้อมกรีน', 'ข้อเสนอแนะด้านสิ่งแวดล้อม')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
     echo "Error: " . $sql . "<br>" . $con->error."<br>";
 }
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 42, 'รณรงค์เลิกสูบบุหรี่ 2', 'https://drive.google.com/file/d/13cPdJKwiS-RN0Sh1iDcXODfzWjeWngOj/view?usp=sharing')";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error."<br>";
-}
+
 
 ?>
