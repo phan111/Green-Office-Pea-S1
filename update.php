839762e2b031c9e52fb2af4ab0699dc4cca60c88
phan111 <chatpimookt@gmail.com>
2019-05-15 13:16:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,10 +1,266 @@
 <?php
 include 'connect.php';
 $sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1omOjXhDpAx9Jtkp36niv_aoAO1FjIdiO/view?usp=sharing"
+        WHERE detail_id = 1';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/17_v30KSY0RHrW1_0V5QkrMrQfJB-ARAE/view?usp=sharing"
+        WHERE detail_id = 2';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/10tGBmXgexxdIZzB4bEQu3T_6wIleyYIf/view?usp=sharing"
+        WHERE detail_id = 30';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1YGdcL2nyj9-QnLqeoxOu7_ctzi5rIQUK/view?usp=sharing"
+        WHERE detail_id = 31';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/17t1BVHZUd4EHe2n6xhPn8mV7L4jYovHV/view?usp=sharing"
+        WHERE detail_id = 34';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1ESSljQfPQtHrfMOVTYazfJO82eK9xmPK/view?usp=sharing"
+        WHERE detail_id = 35';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1hOWcI5us9wTfLcoA-SoqgsEXMYZfrW6R/view?usp=sharing"
+        WHERE detail_id = 36';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1aWB7pDs4ZBwlYcO7vyV0aauB2LgY128_/view?usp=sharing"
+        WHERE detail_id = 37';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1TsNm1FRHWFXnJU8za_AKagJM-6QhkElO/view?usp=sharing"
+        WHERE detail_id = 32';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1pKc7Of5ZXEydtrV3oV7_Ewr1ZyMle_ch/view?usp=sharing"
+        WHERE detail_id = 3';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1Mn2iPvOfGFmsWZInY04enigT4R6ejT0B/view?usp=sharing"
+        WHERE detail_id = 4';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1g6AkYvsR2vvSgg_umk--T3j5N1OQonLa/view?usp=sharing"
+        WHERE detail_id = 5';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1s_TTrBgfInQmtgYqAa0cIgIZEiuIpbYO/view?usp=sharing"
+        WHERE detail_id = 6';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1nZ8_e9PjExOTvGANxeaf7TD66ip3hLmk/view?usp=sharing"
+        WHERE detail_id = 7';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1oNHCVKJkW_7MHnMB2FpXP2VaxywEqDu9/view?usp=sharing"
+        WHERE detail_id = 8';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/15VsWCCgdJXndImSMo-f4yrA5A8G6-m2D/view?usp=sharing"
+        WHERE detail_id = 38';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1yNvpN1UeQByI8KaUhVyQc4J7vQQU2ahO/view?usp=sharing"
+        WHERE detail_id = 39';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/13xW-yhBivEdsZ_GuQq5gMhU01HBvAl_L/view?usp=sharing"
+        WHERE detail_id = 40';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
         SET reply_pc = "https://drive.google.com/file/d/1zCa4BeWOVaTLsmuix_BAFxF6eIlCXQIT/view?usp=sharing"
         WHERE detail_id = 33';
 if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully";
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/10DtUDfuDb5xNQQ5JYt17VI02F7Z9cTjU/view?usp=sharing"
+        WHERE detail_id = 9';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1sxb8Jvbpd2KDluflizJ2zO0d2WB1zf44/view?usp=sharing"
+        WHERE detail_id = 10';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1qARZTKjVJT3z7sv0sEC0Os1dSS7Z8WsW/view?usp=sharing"
+        WHERE detail_id = 11';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1SX6sABn6G-vLv6mumE7CqvLAMXRF3fM3/view?usp=sharing"
+        WHERE detail_id = 12';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1oBWj8Fh8ZXCxGXJS0dI7iFvXL8vwkhlq/view?usp=sharing"
+        WHERE detail_id = 13';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1wy6cLQlcL-sqULQsx6JOkpdCAZ1OaqXK/view?usp=sharing"
+        WHERE detail_id = 14';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1uArk_5dxGjRN0uPiWySnVdxsBfDrF1cR/view?usp=sharing"
+        WHERE detail_id = 15';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/19gLXQACMlxMcSHWgzeMpYaF0cRqhogBW/view?usp=sharing"
+        WHERE detail_id = 16';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1DU2uTm28CI6c9vS43RZXOLO3b6pq0RbE/view?usp=sharing"
+        WHERE detail_id = 17';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1Bfb5Gtl8K-_hOyqjH2MctdEQxKNIa7HP/view?usp=sharing"
+        WHERE detail_id = 18';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1X_L0JxUF0uHO_KmBdTqzdtNntsPK0-r2/view?usp=sharing"
+        WHERE detail_id = 19';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1Wp18qBiiyjdf4WRyVw7LTfEQkupqxGhN/view?usp=sharing"
+        WHERE detail_id = 20';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1NilaJXjinpiJxH4RJ-FEd4rQRTK6xE0C/view?usp=sharing"
+        WHERE detail_id = 21';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = 'UPDATE detail
+        SET reply_pc = "https://drive.google.com/file/d/1Ytp_Q4taDjf-xYaeo8G9XA0ZHm-JPrsb/view?usp=sharing"
+        WHERE detail_id = 17';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
 } else {
    echo "Error updating record: " . mysqli_error($conn);
 }
