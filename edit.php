6464cacc8b9d261f80f96f1ee2cf810706f5ea1c
phan111 <chatpimookt@gmail.com>
2019-05-27 13:55:03 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,11 +1,13 @@
 <?php
 include 'connect.php';
-echo $sql = '
+if(isset($_POST['keyword']){
+$sql = '
   UPDATE '.$_POST['table'].'
   SET topic = "'.$_POST['topic'].'",
   keyword = "'.$_POST['keyword'].'"
   WHERE keyword_id = '.$_POST['id'].'
 ';
+}
 if (mysqli_query($con, $sql)) {
   echo "<script>alert('เสร็จสิ้น');window.location = 'keyword.php';</script>";
 } else {
