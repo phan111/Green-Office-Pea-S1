044ae3486608a3940e5f5568b2e0d3df080a70c3
phan111 <chatpimookt@gmail.com>
2019-05-27 09:40:12 +0700

    Update keyword.php

--- keyword.php
+++ keyword.php
@@ -22,7 +22,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <link rel="stylsheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
 </head>
 
 <div class="container mt-3">
