f9c8a83f2434396784cb9a0647d56e57ba3c4f67
phan111 <chatpimookt@gmail.com>
2019-05-27 13:10:38 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,8 +1,8 @@
 <?php
 include 'connect.php';
 echo $sql = 'UPDATE '.$_POST['table'].'
-        SET keyword = '.$_POST['keyword'].',
-        topic = '.$_POST['topic'].'
+        SET keyword = ''.$_POST['keyword'].'',
+        topic = "'.$_POST['topic'].'"
         WHERE '.$_POST['table'].'_id = '.$_POST['id'].';
 /*if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
