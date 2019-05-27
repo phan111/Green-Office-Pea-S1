d946cb62be00268c550939205970a654f6f649a8
phan111 <chatpimookt@gmail.com>
2019-05-27 13:26:23 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,8 +1,15 @@
 <?php
-echo '<pre>';
-print_r($_POST);
+include 'connect.php';
 echo $sql = '
   UPDATE '.$_POST['table'].'
-  SET topic = '.$_POST['topic'].'
+  SET topic = '.$_POST['topic'].',
+  keyword = '.$_POST['keyword'].'
+  WHERE keyword_id = '.$_POST['id'].'
 ';
+if (mysqli_query($con, $sql)) {
+   echo "<script>alert('เสร็จสิ้น');window.location = 'https://green-office-peas1.herokuapp.com/keyword_edit.php?id=<?=$POST['id']?>';</script>";
+} else {
+   echo "<script>alert("Error updating record: " . mysqli_error($conn));window.location = 'https://green-office-peas1.herokuapp.com/keyword_edit.php?id=<?=$POST['id']?>';</script>";
+}
+mysqli_close($con);
 ?>
