7234402b046fd1e8ac8fdc8fad2f5fd725597538
phan111 <chatpimookt@gmail.com>
2019-05-27 13:11:52 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 echo $sql = 'UPDATE '.$_POST['table'].'
-        SET keyword = ''.$_POST['keyword'].'',
+        SET keyword = "'.$_POST['keyword'].'",
         topic = "'.$_POST['topic'].'"
         WHERE '.$_POST['table'].'_id = '.$_POST['id'].';
 /*if (mysqli_query($con, $sql)) {
