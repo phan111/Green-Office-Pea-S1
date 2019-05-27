2a2a965882959b7abb89c1483a353c05ff5121eb
phan111 <chatpimookt@gmail.com>
2019-05-27 15:19:31 +0700

    Update detail_update.php

--- detail_update.php
+++ detail_update.php
@@ -1,15 +1,3 @@
 <?php
-include 'connect.php';
-$sql = '
-  UPDATE 'detail'
-  SET subtopic = "'.$_POST['subtopic'].'",
-  reply_pc = "'.$_POST['reply_pc'].'"
-  WHERE detail_id = '.$_POST['id'].'
-';
-if (mysqli_query($con, $sql)) {
-  echo "<script>alert('เสร็จสิ้น');window.location = 'detail.php';</script>";
-} else {
-  echo "<script>alert('ผิดพลาด กรุณาลองใหม่');window.location = 'detail.php';</script>";
-}
-mysqli_close($con);
+print_r($_POST);
 ?>
