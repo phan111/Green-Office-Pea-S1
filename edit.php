18bfba0fc8a70d9c6c631cf8f61753962deb7470
phan111 <chatpimookt@gmail.com>
2019-05-27 12:16:14 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,4 +1,13 @@
 <?php
-echo '<pre>';
-print_r($_POST);
+include 'connect.php';
+$sql = 'UPDATE '".$_POST['table']."'
+        SET keyword = '".$_POST['keyword']."',
+        topic = '".$_POST['topic']."'
+        WHERE keyword_id = '".$_POST['id']."'';
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+mysqli_close($con);
 ?>
