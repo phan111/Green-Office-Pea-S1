ee911f5b4f126e819bc0bda32782df2e4f96a50a
phan111 <chatpimookt@gmail.com>
2019-06-24 09:14:40 +0700

    Create search.php

---
+++ search.php
@@ -0,0 +1,15 @@
+<?php
+include 'connect.php';
+$sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.reply_pc 
+	FROM keyword k
+	INNER JOIN detail d
+	ON k.keyword_id = d.keyword_id
+	WHERE keyword LIKE '%{$_GET['key']}%'
+";
+$resource = $con->query($sql);
+$ans = array();
+while ( $rows = $resource->fetch_assoc() ) {
+    $ans[] = $rows;
+    print_r($rows);
+}
+?>
