841dd15032529fb8cc915fd3bcfa92fc1626b5f4
phan111 <chatpimookt@gmail.com>
2019-06-24 13:39:36 +0700

    Update search.php

--- search.php
+++ search.php
@@ -19,8 +19,8 @@ $sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.repl
 ";
 $resource = $con->query($sql);
 while ($rows = $resource->fetch_assoc()) {
-	echo '<div class="row">';
-		echo '<a class="btn btn-success" href="'.$rows['reply_pc'].'">'.$rows['subtopic'].'</a><br>';
+	echo '<div class="row mb-1">';
+		echo '<a class="btn btn-success d-flex justify-content-center" href="'.$rows['reply_pc'].'">'.$rows['subtopic'].'</a><br>';
 	echo '</div>';
 }
 ?>
