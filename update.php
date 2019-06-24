8980a8f651f738bfc844145451de7a19016dc37d
phan111 <chatpimookt@gmail.com>
2019-06-24 15:08:27 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
-$sql = INSERT INTO keyword (keyword_id, keyword, topic )
-VALUES (6, 'Greenofficeข้อ6ผลการใช้ทรัพยากร พลังงาน และของเสียกรีนพฤษภาคมพค', 'ผลการใช้ทรัพยากร พลังงาน และของเสีย')';
+$sql = "INSERT INTO keyword (keyword_id, keyword, topic )
+VALUES (6, 'Greenofficeข้อ6ผลการใช้ทรัพยากร พลังงาน และของเสียกรีนพฤษภาคมพค', 'ผลการใช้ทรัพยากร พลังงาน และของเสีย')";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
