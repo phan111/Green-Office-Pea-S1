ea01fbdb331fa91a60e16cdb2a83febd31685c7e
phan111 <chatpimookt@gmail.com>
2019-06-24 15:54:51 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,7 +1,8 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
-VALUES (13, 'Greenofficeแผนป้องกันและระงับอัคคีภัยกฟต.1กรีน', 'แผนป้องกันและระงับอัคคีภัยกฟต.1')";
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 48, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 1', 'https://drive.google.com/file/d/1Pt0TphTs30i2C5yADURT3X04c5XRuStP/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
@@ -9,7 +10,7 @@ if ($con->query($sql) === TRUE) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (7, 46, 'อุปกรณ์สำนักงาน 9 รายการ (2)', 'https://drive.google.com/file/d/18QtJ6-bsDABagpvb7gr-yoHoiijasqAq/view?usp=sharing')";
+VALUES (13, 49, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 2', 'https://drive.google.com/file/d/1fN_e2YOIqINuTVBsx3kgEcZpenzSTVPP/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
@@ -17,7 +18,39 @@ if ($con->query($sql) === TRUE) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (7, 47, 'อุปกรณ์สำนักงาน 9 รายการ (3)', 'https://drive.google.com/file/d/1I01fRt7cHvzHHxJI4QOwOGWjJOQmnCXz/view?usp=sharing')";
+VALUES (13, 50, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 3', 'https://drive.google.com/file/d/1r1cgY8bMfb771_s3hVAtzptFwjXVqHDP/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 51, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 4', 'https://drive.google.com/file/d/1TP-8qOoc5b9yDyeGSMaeXNOnlzRs6Lft/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 52, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 4(ต่อ)', 'https://drive.google.com/file/d/19VlLFRgkAmPWAPZgFQEvN9haW7HSLpdk/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 53, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 5', 'https://drive.google.com/file/d/1_ZnKt75Cb8Cqmm4GpXx0N2P3S3q4vpc4/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 54, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 6', 'https://drive.google.com/file/d/1pg1m8FeeYq6xeT7AdhZC2q8_t04k6cpV/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
@@ -25,5 +58,12 @@ if ($con->query($sql) === TRUE) {
 }
 
 
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (13, 55, 'แผนป้องกันและระงับอัคคีภัยกฟต.1 แผนที่ 7', 'https://drive.google.com/file/d/1IBvYGiin8GZxrd_LR54FVNUkU1uGxNKR/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
 
 ?>
