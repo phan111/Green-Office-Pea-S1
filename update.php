684a541f7565e3989f08e99bbdf6de27914348ac
phan111 <chatpimookt@gmail.com>
2019-06-24 15:18:18 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
-$sql = "INSERT INTO detail (detail_id, , reply_pc, keyword_id, subtopic, reply )
-VALUES (44, 'https://drive.google.com/file/d/1gcWpB_JhjLJnU3-Wuo0ogpiBDth8ub4t/view?usp=sharing', 6, 'วิธีการคำนวณ', '')";
+$sql = "INSERT INTO detail (detail_id, , subtopic, reply, keyword_id, reply_pc )
+VALUES (44, '', 'วิธีการคำนวณ', 6, 'https://drive.google.com/file/d/1gcWpB_JhjLJnU3-Wuo0ogpiBDth8ub4t/view?usp=sharing')";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
