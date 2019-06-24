74dceb5385891843292aec06d3bd8219d8ef8b0b
phan111 <chatpimookt@gmail.com>
2019-06-24 11:12:52 +0700

    Update search.php

--- search.php
+++ search.php
@@ -1,11 +1,14 @@
-<style>
-a{
-	background:#00cc00;
-	height: 150px;
-	line-height: 150px;
-	border-radius: 5px;
-}
-</style>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</head>
 <?php
 include 'connect.php';
 $sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.reply_pc 
@@ -16,6 +19,6 @@ $sql = "SELECT k.keyword_id, k.keyword, k.topic, d.detail_id, d.subtopic, d.repl
 ";
 $resource = $con->query($sql);
 while ($rows = $resource->fetch_assoc()) {
-	echo '<a href="'.$rows['reply_pc'].'">'.$rows['subtopic'].'</a><br>';
+	echo '<a class="btn btn-success" href="'.$rows['reply_pc'].'">'.$rows['subtopic'].'</a><br>';
 }
 ?>
