4e9ee1e1c89cfb39440ad8c21b9be1ff886d24b8
phan111 <chatpimookt@gmail.com>
2019-06-24 15:33:11 +0700

    Update add.php

--- add.php
+++ add.php
@@ -1,12 +1,29 @@
 <?php
 include 'connect.php';
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 44, 'วิธีการคำนวณ', 'https://drive.google.com/file/d/1gcWpB_JhjLJnU3-Wuo0ogpiBDth8ub4t/view?usp=sharing')";
+VALUES (7, 45, 'อุปกรณ์สำนักงาน 9 รายการ (1)', 'https://drive.google.com/file/d/14OBJoQUgk9xHwUmGRbDSYMlbtSJh1-Lc/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
     echo "Error: " . $sql . "<br>" . $con->error."<br>";
 }
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (7, 46, 'อุปกรณ์สำนักงาน 9 รายการ (2)', 'https://drive.google.com/file/d/18QtJ6-bsDABagpvb7gr-yoHoiijasqAq/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (7, 47, 'อุปกรณ์สำนักงาน 9 รายการ (3)', 'https://drive.google.com/file/d/1I01fRt7cHvzHHxJI4QOwOGWjJOQmnCXz/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
 
 
 ?>
