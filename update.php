5206239fb191f334066c54a03c5afa55103f3d06
phan111 <chatpimookt@gmail.com>
2019-06-25 08:38:26 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,17 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE keyword
-SET keyword = 'Greenofficeข้อ7สินค้าบริการที่เป็นมิตรกับสิ่งแวดล้อมกรีนฉลากสินค้าโรงแรมสีเขียวอุปกรณ์'
-WHERE keyword_id = 7";
+SET keyword = 'Greenofficeข้อ3กฎหมายกฏหมายกรีนการปฎิบัติตามข้อกำหนดด้านสิ่งแวดล้อมต่างๆที่เกี่ยวข้องใกล้ตัวช่องทางค้นคว้าอัคคีภัยแสงเสียงเขตสูบบุหรี่ไฟไหม้'
+WHERE keyword_id = 3";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeแผนป้องกันและระงับอัคคีภัยกฟต.1กรีนไฟไหม้'
+WHERE keyword_id = 3";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
