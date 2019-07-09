88ce61bc0e4e999df6327e4a8d3f8ea8a13f1dd9
phan111 <chatpimookt@gmail.com>
2019-07-09 16:01:42 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,80 @@
 <?php
 include 'connect.php';
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1q8hI8Vww526nYCTrjqw_s_ge3aHBFpJB/view?usp=sharing'
-WHERE detail_id = 2";
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ1นโยบายกรีนone'
+WHERE keyword_id = 1";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ2ปัญหาสิ่งแวดล้อมที่มีนัยสำคัญและการจัดการaspectกรีนtwo'
+WHERE keyword_id = 2";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ3กฎหมายกรีนthree'
+WHERE keyword_id = 3";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ4เป้าหมายและมาตรการพลังงานทรัพยากรไฟฟ้าน้ำมันเชื้อเพลิงกระดาษน้ำกรีนประหยัดหมึกพิมพ์four'
+WHERE keyword_id = 4";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ5ขยะเป้าหมายของเสียมาตรการจัดการของเสียกรีนfive'
+WHERE keyword_id = 5";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ6ผลการใช้ทรัพยากร พลังงาน และของเสียกรีนพฤษภาคมพคsix'
+WHERE keyword_id = 6";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ7สินค้าบริการที่เป็นมิตรกับสิ่งแวดล้อมกรีนฉลากสินค้าโรงแรมสีเขียวอุปกรณ์seven'
+WHERE keyword_id = 7";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ8ก๊าซเรือนกระจกกรีนลดโลกร้อนeight'
+WHERE keyword_id = 8";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ9ทีมงานและอำนาจหน้าที่แต่ละหมวดกรีนคณะกรรมการฝ่ายบริหารnine'
+WHERE keyword_id = 9";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+$sql = "UPDATE keyword
+SET keyword = 'Greenofficeข้อ10รณรงค์สิ่งแวดล้อมความสะอาดความเป็นระเบียบบุหรี่กรีนการจัดการten'
+WHERE keyword_id = 10";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
