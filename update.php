2fc1d5f1736ced514e0084fa493afc6b62c17102
phan111 <chatpimookt@gmail.com>
2019-07-26 13:20:07 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,7 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET subtopic = 'ฉลากสินค้าและบริการ'
+SET reply_pc = 'https://drive.google.com/file/d/1AdYZAagTE6rmq4Qp1mA01MtnIbUvYmrM/view?usp=sharing'
 WHERE detail_id = 14";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
