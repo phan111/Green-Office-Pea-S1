db561f92c3de0a3ab75c69f4b702904f2d181d4c
phan111 <chatpimookt@gmail.com>
2019-07-30 09:46:59 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,9 @@
+<?php
+include 'connect.php';
+$sql = 'DELETE FROM detail WHERE detail_id > 60';
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+?>
