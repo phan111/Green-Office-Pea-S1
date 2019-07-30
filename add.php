4e332cb3f5069e5b2bec13d5b31803c0dade5075
phan111 <chatpimookt@gmail.com>
2019-07-30 09:33:06 +0700

    Update add.php

--- add.php
+++ add.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (10, 58, 'ไม้ประดับดูดสารพิษ', 'https://drive.google.com/file/d/11izhwZcrbFIxccdpMYM6CAGaJ7cA0-Oh/view?usp=sharing')";
+VALUES (6, 60, 'ผลการใช้ทรัพยากร พลังงาน และของเสีย เดือนเมษายน เม.ย.', 'https://drive.google.com/file/d/1DW-NeXH3dtxrOjvXjWVA9nP6zplXquMy/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
@@ -10,7 +10,15 @@ if ($con->query($sql) === TRUE) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (13, 59, 'แผนผังทางหนีไฟ', 'https://drive.google.com/file/d/1Yi3bHZCYoUTX9xaK3jiK_QGTHtxAhoa1/view?usp=sharing')";
+VALUES (6, 61, 'ผลการใช้ทรัพยากร พลังงาน และของเสีย เดือนพฤษภาคม พ.ค.', 'https://drive.google.com/file/d/1Ij00nZ7q9gtFVU1dJOXkKIvOYZwH9ImT/view?usp=sharing')";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (6, 62, 'ผลการใช้ทรัพยากร พลังงาน และของเสีย เดือนมิถุนายน มิ.ย.', 'https://drive.google.com/file/d/1lFpYzcXNTor09g_EXCizlhQRRZXd4K23/view?usp=sharing')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
