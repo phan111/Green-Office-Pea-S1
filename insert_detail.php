fe82fd5b0e949545cc5b64f34f74657a0d4fe7fb
phan111 <chatpimookt@gmail.com>
2019-08-26 11:36:23 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -1,14 +1,31 @@
 <?php
 include 'connect.php';
 echo '<pre>';
-print_r($_GET);
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES ($_GET['keyword_id'], $_GET['detail_id'], $_GET['subtopic'], $_GET['reply_pc'])";
+VALUES (4, 63, 'มาตรการการใช้ห้องประชุม', 'https://drive.google.com/file/d/1JhupWEZ1No4Hqqch2y5y2ijd711tlOHu/view?usp=sharing')";
 
-/*if (mysqli_query($con, $sql)) {
+if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}*/
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (4, 64, 'มาตรการในการประชุม', 'https://drive.google.com/file/d/1JhupWEZ1No4Hqqch2y5y2ijd711tlOHu/view?usp=sharing')";
+
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (10, 65, 'มาตรการพาหะนำเชื้อ', 'https://drive.google.com/file/d/1gPjL2e57-TWO3r94xLwOyMuc0jiahXk0/view?usp=sharing')";
+
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
 
 ?>
