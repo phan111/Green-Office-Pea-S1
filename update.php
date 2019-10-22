e39b2ed6c6093f90157a48154cb8e0e6b34ace61
phan111 <chatpimookt@gmail.com>
2019-10-22 13:40:25 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1OeN0s7ER_OBLDch4cq16RbnjCT5OQ5G1/view?usp=sharing'
-WHERE detail_id = 1";
+SET reply_pc = 'https://drive.google.com/file/d/1yXywnyCIJBp5mvZpRGvWLkp_sn0u6CNs/view?usp=sharing'
+WHERE detail_id = 22";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
@@ -10,8 +10,8 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1g6cognNw21ZkH5mQh9qHBaXFW8R99bJT/view?usp=sharing'
-WHERE detail_id = 32";
+SET reply_pc = 'https://drive.google.com/file/d/1-WYv1x-MOJK4IGF0LIz8zs6bjvHIk3LY/view?usp=sharing'
+WHERE detail_id = 23";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
@@ -19,8 +19,8 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1uovsp_23aC0q8sLpwQfEyLOR7k1gxGIA/view?usp=sharing'
-WHERE detail_id = 3";
+SET reply_pc = 'https://drive.google.com/file/d/1aaovfPahpJNWfZNIoZV8UnasEKZ7lV04/view?usp=sharing'
+WHERE detail_id = 24";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
@@ -28,8 +28,8 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1hbvwUTWDRLUmLKgD59HzDLfitWdRLEAH/view?usp=sharing'
-WHERE detail_id = 5";
+SET reply_pc = 'https://drive.google.com/file/d/1bz3LFghiUHNoq_KFCdSl67f2AqIwChIX/view?usp=sharing'
+WHERE detail_id = 25";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
@@ -37,8 +37,8 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/16tYGww1-teO1McF7s0sq4DJ6mW22n6qU/view?usp=sharing'
-WHERE detail_id = 6";
+SET reply_pc = 'https://drive.google.com/file/d/1HnOxlL6-N6mKgrxZ_-vYRbyy_dEW0c7Q/view?usp=sharing'
+WHERE detail_id = 26";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
@@ -46,8 +46,17 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1ZMGYaG1sT5dqKjfo20QSi1HeIvhLa9D2/view?usp=sharing'
-WHERE detail_id = 7";
+SET reply_pc = 'https://drive.google.com/file/d/1w9Ksy3UjCUQ-yQSSKaIDpPjZmB0rj3tY/view?usp=sharing'
+WHERE detail_id = 27";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+
+$sql = "UPDATE detail
+SET reply_pc = 'https://drive.google.com/file/d/1qwL4fx1bSyxmMuUp5Xqeb9VxbG_FUPBb/view?usp=sharing'
+WHERE detail_id = 28";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
