5099ef0f3bac5e5c647fa3d9b1cb68141ac3d144
phan111 <chatpimookt@gmail.com>
2019-11-20 16:02:49 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -1,18 +1,18 @@
 <?php
 include 'connect.php';
 echo '<pre>';
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+/*$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
 VALUES (3, 66, 'กฎหมายเกี่ยวกับสำนักงานสีเขียว', 'https://drive.google.com/file/d/1y1IWyhSHZ581Rvblm9Pnjfrpmh3ELEUH/view?usp=sharing')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+}*/
 
 
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 81, 'วิธีการคำนวณก๊าซเรือนกระจก', 'https://drive.google.com/file/d/1gcWpB_JhjLJnU3-Wuo0ogpiBDth8ub4t/view?usp=sharing')";
+$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
+VALUES (14, 'Greenofficeโครงการบริหารจัดการน้ำเสียภายในพื้นที่สำนักงาน กฟต.1.', 'โครงการบริหารจัดการน้ำเสียภายในพื้นที่สำนักงาน กฟต.1.')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
