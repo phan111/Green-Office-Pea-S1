70e304fee681b3fe8df2c72e076a4756fc3a036a
phan111 <chatpimookt@gmail.com>
2019-11-20 16:16:00 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,26 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET subtopic = 'ปริมาณของเสียที่นำกลับมาใช้ใหม่ในแต่ละเดือน'
-WHERE detail_id = 79";
+SET keyword_id = 14
+WHERE detail_id = 82";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+
+$sql = "UPDATE detail
+SET keyword_id = 14
+WHERE detail_id = 83";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
+}
+
+$sql = "UPDATE detail
+SET keyword_id = 14
+WHERE detail_id = 84";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
