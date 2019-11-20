a3d0d4961e9b8fef64e773a5827b809a64cf8aef
phan111 <chatpimookt@gmail.com>
2019-11-20 15:59:04 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,69 +1,13 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก เม.ย.'
-WHERE detail_id = 68";
+SET subtopic = 'ปริมาณของเสียที่นำกลับมาใช้ใหม่ในแต่ละเดือน'
+WHERE detail_id = 79";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
    echo "Error updating record: " . mysqli_error($conn);
 }
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก พ.ค.'
-WHERE detail_id = 69";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก มิ.ย.'
-WHERE detail_id = 70";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก ก.ค.'
-WHERE detail_id = 71";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก ส.ค.'
-WHERE detail_id = 72";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก ก.ย.'
-WHERE detail_id = 73";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET subtopic = 'ผลการเกิดก๊าซเรือนกระจก ต.ค.'
-WHERE detail_id = 74";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-
 
 
 mysqli_close($con);
