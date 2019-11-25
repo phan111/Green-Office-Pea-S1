37f947f88e803f180d59287af1265a0b3d7ad93c
phan111 <chatpimookt@gmail.com>
2019-11-25 15:10:02 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,32 +1,13 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET keyword_id = 14
-WHERE detail_id = 82";
+SET keyword_id = 5
+WHERE detail_id = 65";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
    echo "Error updating record: " . mysqli_error($conn);
 }
-
-$sql = "UPDATE detail
-SET keyword_id = 14
-WHERE detail_id = 83";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET keyword_id = 14
-WHERE detail_id = 84";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
 
 mysqli_close($con);
 ?>
