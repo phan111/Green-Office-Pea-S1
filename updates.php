a524e9d2cdd18ba0f96667162f218748873c816d
phan111 <chatpimookt@gmail.com>
2020-02-02 22:49:04 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,23 +1,7 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail SET 
-		detail_id = 85
-		WHERE detail_id = 105";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-
-$sql = "UPDATE detail SET 
 		detail_id = 75
-		WHERE detail_id = 85";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-
-$sql = "UPDATE detail SET 
-		detail_id = 106
 		WHERE detail_id = 86";
 $query = mysqli_query($con,$sql);
 if($query) {
@@ -25,13 +9,6 @@ if($query) {
 }
 
 
-$sql = "UPDATE detail SET 
-		detail_id = 76
-		WHERE detail_id = 86";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
 
 
 ?>
