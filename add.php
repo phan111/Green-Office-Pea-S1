a76652c892bea35fbf58c817cba4b4bffd3c57ee
phan111 <chatpimookt@gmail.com>
2020-02-02 10:53:50 +0700

    Update add.php

--- add.php
+++ add.php
@@ -2,15 +2,7 @@
 include 'connect.php';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 85, 'ผลการเกิดก๊าซเรือนกระจก พ.ย.', 'https://drive.google.com/open?id=1EcOjRlEFKm0KbgjoNg_tMlRVqOBwZ4x2')";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error."<br>";
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 61, 'ผลเดือนพฤษภาคม พ.ค.', 'https://drive.google.com/open?id=1zoamebN3p9dYgEivdfmoeLXvtmUGGvaZ')";
+VALUES (6, 86, 'ผลการเกิดก๊าซเรือนกระจก ธ.ค. 'https://drive.google.com/open?id=1zoamebN3p9dYgEivdfmoeLXvtmUGGvaZ')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
