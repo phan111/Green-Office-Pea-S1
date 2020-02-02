860071793e065d8ed3b94c94edba88081ca4d2ef
phan111 <chatpimookt@gmail.com>
2020-02-02 23:03:34 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,14 +1,73 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail SET 
-		detail_id = 86
-		WHERE detail_id = 106";
+		detail_id = 100
+		WHERE detail_id = 75";
 $query = mysqli_query($con,$sql);
 if($query) {
  echo "Record update successfully<br>";
 }
-
-
-
-
+$sql = "UPDATE detail SET 
+		detail_id = 101
+		WHERE detail_id = 76";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 102
+		WHERE detail_id = 77";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 103
+		WHERE detail_id = 78";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 104
+		WHERE detail_id = 79";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 105
+		WHERE detail_id = 80";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 106
+		WHERE detail_id = 81";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 107
+		WHERE detail_id = 82";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 108
+		WHERE detail_id = 83";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
+$sql = "UPDATE detail SET 
+		detail_id = 109
+		WHERE detail_id = 84";
+$query = mysqli_query($con,$sql);
+if($query) {
+ echo "Record update successfully<br>";
+}
 ?>
