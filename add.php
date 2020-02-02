cf376c8f402b1136b0e3e8c0cd99e1e35dd90e63
phan111 <chatpimookt@gmail.com>
2020-02-02 10:49:52 +0700

    Update add.php

--- add.php
+++ add.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 60, 'ผลเดือนเมษายน เม.ย.', 'https://drive.google.com/file/d/1DW-NeXH3dtxrOjvXjWVA9nP6zplXquMy/view?usp=sharing')";
+VALUES (6, 85, 'ผลการเกิดก๊าซเรือนกระจก พ.ย.', 'https://drive.google.com/open?id=1EcOjRlEFKm0KbgjoNg_tMlRVqOBwZ4x2')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
@@ -10,15 +10,7 @@ if ($con->query($sql) === TRUE) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 61, 'ผลเดือนพฤษภาคม พ.ค.', 'https://drive.google.com/file/d/1Ij00nZ7q9gtFVU1dJOXkKIvOYZwH9ImT/view?usp=sharing')";
-if ($con->query($sql) === TRUE) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . $con->error."<br>";
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 62, 'ผลเดือนมิถุนายน มิ.ย.', 'https://drive.google.com/file/d/1lFpYzcXNTor09g_EXCizlhQRRZXd4K23/view?usp=sharing')";
+VALUES (6, 61, 'ผลเดือนพฤษภาคม พ.ค.', 'https://drive.google.com/open?id=1zoamebN3p9dYgEivdfmoeLXvtmUGGvaZ')";
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
