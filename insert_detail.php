f801e53ea99bb906e5d8b9fb4beb5114e48c231b
phan111 <chatpimookt@gmail.com>
2020-02-07 13:49:45 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -2,7 +2,7 @@
 include 'connect.php';
 echo '<pre>';
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (13, 82, 'ขออนุมัติ โครงการบริหารจัดการน้ำเสีย', 'https://drive.google.com/file/d/1Vf3gz6i40830xjCCjyIg35Wk_5JJ_EJj/view?usp=sharing')";
+VALUES (10, 97, 'แก้วเก็บความร้อน', 'https://drive.google.com/drive/u/1/folders/1jTFi7c2x5yi0n9QY5LlGnc6ZTAqg5FFP')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
@@ -11,7 +11,7 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (13, 83, 'โครงการบริหารจัดการน้ำเสียภายในพื้นที่', 'https://drive.google.com/file/d/1DXZBbiVTcOjklKqyHg_ojUOvk-lhTHBw/view?usp=sharing')";
+VALUES (1ภ, 98, 'โครงการบ่อบำบัดน้ำเสีย', 'https://drive.google.com/drive/u/1/folders/1jTFi7c2x5yi0n9QY5LlGnc6ZTAqg5FFP')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
@@ -19,14 +19,6 @@ if (mysqli_query($con, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (13, 84, 'สรุปผลโครงการบริหารจัดการน้ำเสียภายในพื้นที่', 'https://drive.google.com/file/d/1p9-SnW2dCgyuLybnGVgTTmBmjpy37YXG/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 
 /*$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
