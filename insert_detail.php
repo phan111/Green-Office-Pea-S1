b4ea66501df9319be08893b957d630b73f52625a
phan111 <chatpimookt@gmail.com>
2020-02-07 13:51:47 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -1,17 +1,10 @@
 <?php
 include 'connect.php';
 echo '<pre>';
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (10, 97, 'แก้วเก็บความร้อน', 'https://drive.google.com/drive/u/1/folders/1jTFi7c2x5yi0n9QY5LlGnc6ZTAqg5FFP')";
 
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (1ภ, 98, 'โครงการบ่อบำบัดน้ำเสีย', 'https://drive.google.com/drive/u/1/folders/1jTFi7c2x5yi0n9QY5LlGnc6ZTAqg5FFP')";
+VALUES (14, 98, 'โครงการบ่อบำบัดน้ำเสีย', 'https://drive.google.com/drive/u/1/folders/1jTFi7c2x5yi0n9QY5LlGnc6ZTAqg5FFP')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
