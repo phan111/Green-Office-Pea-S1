e2dd6b9655c451749b3a89f8289d5b7611440122
phan111 <chatpimookt@gmail.com>
2020-02-07 13:56:01 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,15 +1,15 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail SET 
-		detail_id = 87
-		WHERE detail_id = 100";
+		reply_pc = 'https://drive.google.com/open?id=13ZO0-7uDRmwP2ttOGAnd_Jlo6dCeqNKV'
+		WHERE detail_id = 97";
 $query = mysqli_query($con,$sql);
 if($query) {
  echo "Record update successfully<br>";
 }
 $sql = "UPDATE detail SET 
-		detail_id = 88
-		WHERE detail_id = 101";
+		detail_id = 'https://drive.google.com/open?id=13ZO0-7uDRmwP2ttOGAnd_Jlo6dCeqNKV'
+		WHERE detail_id = 98";
 $query = mysqli_query($con,$sql);
 if($query) {
  echo "Record update successfully<br>";
@@ -21,53 +21,5 @@ $query = mysqli_query($con,$sql);
 if($query) {
  echo "Record update successfully<br>";
 }
-$sql = "UPDATE detail SET 
-		detail_id = 90
-		WHERE detail_id = 103";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 91
-		WHERE detail_id = 104";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 92
-		WHERE detail_id = 105";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 93
-		WHERE detail_id = 106";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 94
-		WHERE detail_id = 107";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 95
-		WHERE detail_id = 108";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
-$sql = "UPDATE detail SET 
-		detail_id = 96
-		WHERE detail_id = 109";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
-}
+
 ?>
