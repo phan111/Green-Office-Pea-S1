0e37e68b877a2005603c910b48be4d16bef12efc
phan111 <chatpimookt@gmail.com>
2020-02-18 09:28:19 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -4,7 +4,7 @@ echo '<pre>';
 
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (14, 98, 'รณรงค์การลดขยะ', 'https://drive.google.com/open?id=1BZZLs3SoS_VyfQ6l1iHB6UcGNdE3GkOR')";
+VALUES (14, 99, 'QR Code โครงการบ่อบำบัดน้ำเสีย กฟต.1', 'https://drive.google.com/open?id=1istvCd6P0OwTOCHaiWA8-2kTMRYzah8P')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
