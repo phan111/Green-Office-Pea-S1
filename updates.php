03a4c1306d54c5074c8fbef5b4dd2b113d6f0dd4
phan111 <chatpimookt@gmail.com>
2020-02-18 09:38:49 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,12 +1,13 @@
 <?php
 include 'connect.php';
-$sql = "UPDATE detail SET 
-		subtopic = 'แก้วเก็บความร้อน/ความร้อน'
-		WHERE detail_id = 97";
-$query = mysqli_query($con,$sql);
-if($query) {
- echo "Record update successfully<br>";
+$sql = "UPDATE detail
+SET subtopic = "แก้วกักเก็บความเย็น"
+WHERE detail_id = 97";
+if (mysqli_query($con, $sql)) {
+   echo "Record updated successfully<br>";
+} else {
+   echo "Error updating record: " . mysqli_error($conn);
 }
 
-
+mysqli_close($con);
 ?>
