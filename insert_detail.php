56d616f94f7b968146ef6ca7ac5fe5e513bf2576
phan111 <chatpimookt@gmail.com>
2020-05-20 10:57:35 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -4,7 +4,16 @@ echo '<pre>';
 
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (14, 100, 'ผังและพื้นที่ สนง.กฟต.1', 'https://drive.google.com/open?id=15vpLUDer8_JIiB4ydWZK1dTUh18_c9kd')";
+VALUES (5, 101, 'การทิ้งขยะในพื้นที่อาคาร', 'https://drive.google.com/open?id=1eJiI7q4dQtuukpx-XhmaSd3ywEs1Q-eb')";
+
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
+VALUES (5, 102, 'มาตรการควบคุมสัตว์พาหะนำเชื้อ', 'https://drive.google.com/open?id=14VRJbEgETCb7hriS8M91wMuKKPDuQWJR')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
