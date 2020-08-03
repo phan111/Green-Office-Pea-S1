2b56ee239a08c6c2103cc20f79d7c72529a124aa
phan111 <chatpimookt@gmail.com>
2020-08-03 09:44:22 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,17 +1,8 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/open?id=1RUAeP4AWH63Qhg0R-gO0wTt989uPbgnW'
-WHERE detail_id = 27";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/open?id=1fQJfSOJSAd3w6aJFZNNs-T7exansYa_v'
-WHERE detail_id = 22";
+SET subtopic = 'การคำนวนก๊าซเรือนกระจก'
+WHERE detail_id = 103";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
