94976a7f35b4c60b95892889f9c8474596955d8b
phan111 <chatpimookt@gmail.com>
2020-11-13 10:58:02 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,8 +1,8 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/open?id=1nb1g2GFtb-CSi_tcIVDirsW6VUedzM2T'
-WHERE detail_id = 30";
+SET reply_pc = 'https://drive.google.com/file/d/16wmAy4mb_LAkdvCrNTunQnTe2aNWplG6/view?usp=sharing'
+WHERE detail_id = 64";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
