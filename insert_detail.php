30322147fa1e4c7bb92356248a3c246b57d5f56b
phan111 <chatpimookt@gmail.com>
2020-11-13 11:30:54 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -2,17 +2,12 @@
 include 'connect.php';
 echo '<pre>';
 
-
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 116, 'แผนการจัดการนก', 'https://drive.google.com/file/d/1-sRpiuYDo87VQxfzZWOG3SRJ9yY0EUcG/view?usp=sharing')";
-
+VALUES (4, 117, 'การจำแนกประเภทขยะ', 'https://drive.google.com/file/d/1I4LXzP9q2UMBmMVzJ5Ej3lnY9ECVAYO4/view?usp=sharing')";
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 117, 'การจำแนกประเภทขยะ', 'https://drive.google.com/file/d/1I4LXzP9q2UMBmMVzJ5Ej3lnY9ECVAYO4/view?usp=sharing')";
 
 ?>
