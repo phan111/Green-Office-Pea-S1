e8926edb2eb58bc3116cb8a7334f64b0342b3489
phan111 <chatpimookt@gmail.com>
2020-11-13 11:26:35 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -4,7 +4,7 @@ echo '<pre>';
 
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 109, 'มาตรการรณรงค์ไม่สูบบุหรี่', 'https://drive.google.com/file/d/1UKtfhmn44-g8XGJLjxD3cU5hM9Tlywso/view?usp=sharing')";
+VALUES (4, 116, 'แผนการจัดการนก', 'https://drive.google.com/file/d/1-sRpiuYDo87VQxfzZWOG3SRJ9yY0EUcG/view?usp=sharing')";
 
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
@@ -13,49 +13,6 @@ if (mysqli_query($con, $sql)) {
 }
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 110, 'มาตรการในการประหยัดพลังงาน', 'https://drive.google.com/file/d/1Ii92aN7tIhRVCpoVPnMpArUYyfSlpaI6/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 112, 'มาตรการในการประหยัดน้ำ', 'https://drive.google.com/file/d/1z0EllNM0gpbog2LFZhH_cedWutGR-dZm/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 113, 'มาตรการในการประหยัดทรัพยากร', 'https://drive.google.com/file/d/1i82Tu03QyFdN41R38IR6et3HdM0f7ImQ/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 114, 'มาตรการในการประหยัดไฟฟ้า', 'https://drive.google.com/file/d/1i82Tu03QyFdN41R38IR6et3HdM0f7ImQ/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 115, 'มาตรการในการประหยัดน้ำมันเชื้อเพลิง', 'https://drive.google.com/file/d/1VQTo1LbamtxTI5ve4A6QTkY5fY_LX0N6/view?usp=sharing')";
-
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
+VALUES (4, 117, 'การจำแนกประเภทขยะ', 'https://drive.google.com/file/d/1I4LXzP9q2UMBmMVzJ5Ej3lnY9ECVAYO4/view?usp=sharing')";
 
 ?>
