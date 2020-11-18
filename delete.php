9063ad02d91f5f1f696e1d02a7836f22bbf6845b
phan111 <chatpimookt@gmail.com>
2020-11-18 16:41:30 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,6 +1,12 @@
 <?php
 include 'connect.php';
-$sql = 'DELETE FROM detail WHERE detail_id = 118';
+$sql = 'DELETE FROM detail WHERE detail_id = 124';
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error."<br>";
+}
+$sql = 'DELETE FROM keyword WHERE keyword_id = 15';
 if ($con->query($sql) === TRUE) {
     echo "New record created successfully<br>";
 } else {
