72c947cfa12202a6aa474525fc6aba637627bab6
phan111 <chatpimookt@gmail.com>
2020-11-26 09:57:56 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,53 +1,8 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1555c7HR98BSgumbGD4QhB_ospzOuAMRw/view?usp=sharing'
-WHERE detail_id = 23";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1KT_hIFOewEwU-XsFffaydST4jjShZv8B/view?usp=sharing'
-WHERE detail_id = 24";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/147FhSZuij2JFEXEHNP_y6-Ri8nFa1Rj8/view?usp=sharing'
-WHERE detail_id = 25";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1uXXWRwv_blVQUZuc582UXpI5fXGvTih5/view?usp=sharing'
-WHERE detail_id = 26";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/13SAqfSdeua7uPY3QOuanVzhEgAVkrh9m/view?usp=sharing'
-WHERE detail_id = 27";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
-
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1vu7830ZRVsqU_M2vUiLPD9Fv5mmrj6O9/view?usp=sharing'
-WHERE detail_id = 28";
+SET reply_pc = 'https://drive.google.com/file/d/1UK6Nf-FJTx-LSMEXDaakQFwYa163oJGU/view?usp=sharing'
+WHERE detail_id = 125";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
