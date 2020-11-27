bd7d832aee0782db251204f3cc7388ed4022afa7
phan111 <chatpimookt@gmail.com>
2020-11-27 10:45:16 +0700

    Update updates.php

--- updates.php
+++ updates.php
@@ -1,21 +1,14 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1g4uKUlRb4onpwCvQPv4T8dfmsUWBFbv_/view?usp=sharing'
-WHERE detail_id = 106";
+SET detail_id = 125
+WHERE detail_id = 999";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
    echo "Error updating record: " . mysqli_error($conn);
 }
-$sql = "UPDATE detail
-SET reply_pc = 'https://drive.google.com/file/d/1AHmtTmsANil4umeEsovagWIVKlC9ZKNS/view?usp=sharing'
-WHERE detail_id = 107";
-if (mysqli_query($con, $sql)) {
-   echo "Record updated successfully<br>";
-} else {
-   echo "Error updating record: " . mysqli_error($conn);
-}
+
 
 mysqli_close($con);
 ?>
