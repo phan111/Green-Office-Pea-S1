94350d2be3c7eed6217178bf68d55306db7fc0fe
phan111 <chatpimookt@gmail.com>
2020-12-07 15:46:26 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -3,27 +3,12 @@ include 'connect.php';
 echo '<pre>';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (4, 127, 'การจัดประชุม อบรม สัมนา กฟต.1', 'https://drive.google.com/file/d/1xsCyXVTA0Kw5d-IPtuDNW_hKCXeDb4S-/view?usp=sharing')";
+VALUES (41, 129, 'โครงการหลังคาพลังงานแสงอาทิตย์', 'https://drive.google.com/file/d/1jEMrrTmR5-u-KM2_kGqvXx6WLpIlarGx/view?usp=sharing')";
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-$sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (3, 128, 'กฎหมายที่เกี่ยวข้อง', 'https://drive.google.com/file/d/16kvnHiwFtUdPMldCXyiST0EXeqR5qlEl/view?usp=sharing')";
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-/*$sql = "INSERT INTO keyword (keyword_id, keyword, topic)
-VALUES (15, 'คณะกรรมการฝ่ายบริหารคณะทีมงาน', 'คณะกรรมการ')";
-if (mysqli_query($con, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}*/
 
 ?>
