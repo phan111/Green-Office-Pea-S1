15e187db7c2ca5f29279521598a923ff3069a1b8
phan111 <chatpimookt@gmail.com>
2020-12-23 09:39:44 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -3,7 +3,7 @@ include 'connect.php';
 echo '<pre>';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (8, 130, 'ตุลาคม 2563', 'https://drive.google.com/file/d/1fLe5dpQz9lfqlADnG1paKU88KwAT3v0v/view?usp=sharing')";
+VALUES (6, 131, 'สรุปผลการใช้ห้องประชุม', 'https://drive.google.com/file/d/1FiVBxI_ZK_CmuKyu-MTUvx4XHJ-WqhEc/view?usp=sharing')";
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
