424a068021b09470717eccd5384f2fed3acdf73f
phan111 <chatpimookt@gmail.com>
2021-05-25 16:26:59 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,8 +1,8 @@
 <?php
 include 'connect.php';
 $sql = "UPDATE detail
-SET subtopic = 'การคำนวนก๊าซเรือนกระจก'
-WHERE detail_id = 103";
+SET reply_pc = 'https://drive.google.com/file/d/1upCQxALu1we2KGutpvae6EqQEo1Ycav8/view?usp=sharing'
+WHERE detail_id = 30";
 if (mysqli_query($con, $sql)) {
    echo "Record updated successfully<br>";
 } else {
