dff44864e63efa444c9330aa155fd512cef79794
phan111 <chatpimookt@gmail.com>
2022-10-05 16:19:24 +0700

    Update insert_detail.php

--- insert_detail.php
+++ insert_detail.php
@@ -3,7 +3,37 @@ include 'connect.php';
 echo '<pre>';
 
 $sql = "INSERT INTO detail (keyword_id, detail_id, subtopic, reply_pc)
-VALUES (6, 131, 'สรุปผลการใช้ห้องประชุม', 'https://drive.google.com/file/d/1FiVBxI_ZK_CmuKyu-MTUvx4XHJ-WqhEc/view?usp=sharing')";
+VALUES (6, 223, 'ผลเดือนมกราคม 2565', 'https://drive.google.com/file/d/19FVPi7L_fp_FI5oOefXalh83j2a9prmW/view?usp=sharing')";
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+VALUES (6, 224, 'ผลเดือนกุมภาพัน 2565', 'https://drive.google.com/file/d/1qwZHMxqp9pGMn2_r9NXJPwCibNPEIA1J/view?usp=sharing')";
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+VALUES (6, 224, 'ผลเดือนมีนาคม 2565', 'https://drive.google.com/file/d/1QNRPbWF7cPXD2Lr9BZ0olxddNmqc8FLM/view?usp=sharing')";
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+VALUES (6, 225, 'ผลเดือนเมษายน 2565', 'https://drive.google.com/file/d/1W3O3AnPxdvFAxQnkVW8zPCc2x1ZulYUP/view?usp=sharing')";
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+VALUES (6, 226, 'ผลเดือนพฤษภาคม 2565', 'https://drive.google.com/file/d/1K9LY0ndzd4nK4gCeZAw7D0WLfq4xsMx9/view?usp=sharing')";
+if (mysqli_query($con, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+VALUES (6, 227, 'ผลเดือนมิถุนายน 2565', 'https://drive.google.com/file/d/1pAfgp_4t32NBjW_Dwqh1dgXuVxTuUmIm/view?usp=sharing')";
 if (mysqli_query($con, $sql)) {
     echo "New record created successfully";
 } else {
