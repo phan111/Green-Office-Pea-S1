2a0204accfbdb9ced13c2c938fbd42382a377376
phan111 <chatpimookt@gmail.com>
2022-12-06 14:23:07 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
+<?php include 'connect.php' ?>
 <html>
   <head></head>
   <body>
-      <input type="text">
-      <input type="submit">
+      <form action="#" method="get">
+        <input type="text" name="word">
+        <input type="submit" value="ค้นหา">
+      </form>
+
+      <?php
+      if(isset($_GET['word']){
+        print_r($_GET);
+      ?>
   </body>
 </html>
