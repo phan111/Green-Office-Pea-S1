27a023d9ca67723465155e8fba0bb89fe27e1613
phan111 <chatpimookt@gmail.com>
2022-12-06 14:25:31 +0700

    Update test.php

--- test.php
+++ test.php
@@ -6,10 +6,5 @@
         <input type="text" name="word">
         <input type="submit" value="ค้นหา">
       </form>
-
-      <?php
-      if(isset($_GET['word']){
-        print_r($_GET);
-      ?>
   </body>
 </html>
